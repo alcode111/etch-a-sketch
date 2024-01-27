eaa2f694bedf6cb7f935d8d71dfe992d3f6cf072
Ismail <141173099+alcode111@users.noreply.github.com>
2024-01-27 16:22:15 +0100

    Implement a function that creates a grid based on a user input number, call it and have it default to 16 initially

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const gridContainer = document.getElementById("grid-container")
 const newGridBtn = document.getElementById("new-grid-btn")
 const clearGridBtn = document.getElementById("clear-grid-btn")
 
+function createGrid(userInput) {
+    for (let i = 0; i < userInput; i++) {
+        const row = document.createElement("div")
+
+        for (let j = 0; j < userInput; j++) {
+            const square = document.createElement("div")
+            square.classList.add("square")
+            row.append(square)
+        }
+        gridContainer.append(row)
+    }
+}
+
+createGrid(16)
