46f3c65719d9ded2bb51566d5711069a87d79f81
Ismail <141173099+alcode111@users.noreply.github.com>
2024-01-27 14:29:55 +0100

    Select the buttons and grid container for future DOM manipulation

---
+++ index.js
@@ -0,0 +1,4 @@
+const gridContainer = document.getElementById("grid-container")
+const newGridBtn = document.getElementById("new-grid-btn")
+const clearGridBtn = document.getElementById("clear-grid-btn")
+
