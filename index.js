59c1c237e3e98ab41d384c17347eb16183c0861c
Ismail <141173099+alcode111@users.noreply.github.com>
2024-01-27 16:37:20 +0100

    Add event listener to New Grid button to prompt user for grid size, clear existing grid and create a new grid based on user input

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ function clearGrid() {
     gridContainer.innerHTML = ""
 }
 
+newGridBtn.addEventListener('click', function() {
+    const gridNumberPrompt = prompt("Please select a grid number between 1 and 100.")
+    let userInput = parseInt(gridNumberPrompt)
+
+    if (!isNaN(userInput) && userInput > 0 && userInput < 100) {
+        clearGrid()
+        createGrid(userInput)
+    } else {
+        alert("Invalid input. Please enter a valid number between 1 and 100.")
+    }
+})
+
 function createGrid(userInput) {
     for (let i = 0; i < userInput; i++) {
         const row = document.createElement("div")
